073abee10b8807585040760fa00d025feae3d818
zch <zhangchenghui.dev@gmail.com>
2018-01-31 23:29:34 +0800

    first commit

---
+++ src/main/java/com/objcoding/springbootstarter/FlymqAutoConfiguration.java
@@ -0,0 +1,20 @@
+package com.objcoding.springbootstarter;
+
+import com.objcoding.flymq.utils.JedisManager;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by chenghui.zhang on 2018/1/31.
+ */
+@Configuration
+@ConditionalOnClass(JedisManager.class)
+@EnableConfigurationProperties(FlymqProperties.class)
+public class FlymqAutoConfiguration {
+
+    public FlymqAutoConfiguration(FlymqProperties flymqProperties) {
+//        System.out.println("====== 初始化连接池 =======");
+        JedisManager.initPool(flymqProperties);
+    }
+}
---
+++ src/main/java/com/objcoding/springbootstarter/FlymqProperties.java
@@ -0,0 +1,14 @@
+package com.objcoding.springbootstarter;
+
+import com.objcoding.flymq.config.RedisConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * redis配置类
+ *
+ * Created by chenghui.zhang on 2018/02/01.
+ */
+@ConfigurationProperties(prefix = "flymq")
+public class FlymqProperties extends RedisConfig {
+
+}
